379a8e91b52517c0d21f32669dab2ba79eafe742
AnaMariaMoga <anamoga435@gmail.com>
2022-02-18 10:29:52 +0200

    fix: improve e2e tests

--- cluster/local/integration_tests.sh
+++ cluster/local/integration_tests.sh
@@ -221,7 +221,8 @@ EOF
 
 echo "${INSTALL_CRED_YAML}" | "${KUBECTL}" apply -f -
 
-echo_step "deploy datacenter"
+## Datacenter CR Tests
+echo_step "deploy a datacenter CR"
 INSTALL_DC_YAML="$( cat <<EOF
 apiVersion: compute.ionoscloud.crossplane.io/v1alpha1
 kind: Datacenter
@@ -238,12 +239,41 @@ EOF
 
 echo "${INSTALL_DC_YAML}" | "${KUBECTL}" apply -f -
 
-echo_step "waiting for cr datacenter example to be ready"
+echo_step "waiting for datacenter CR to be ready"
 kubectl wait --for=condition=ready datacenters/example
 
-echo_step "get datacenters and describe datacenter"
+echo_step "get datacenters and describe datacenter CR"
 kubectl get datacenters
 kubectl describe datacenters example
+
+echo_step "update datacenter CR"
+INSTALL_DC_YAML="$( cat <<EOF
+apiVersion: compute.ionoscloud.crossplane.io/v1alpha1
+kind: Datacenter
+metadata:
+  name: example
+spec:
+  forProvider:
+    name: Test Datacenter CR
+    location: de/txl
+    description: e2e crossplane testing
+  providerConfigRef:
+    name: example
+EOF
+)"
+
+echo "${INSTALL_DC_YAML}" | "${KUBECTL}" apply -f -
+
+echo_step "waiting for datacenter CR to be ready"
+kubectl wait --for=condition=ready datacenters/example
+
+echo_step "uninstalling datacenter CR"
+echo "${INSTALL_DC_YAML}" | "${KUBECTL}" delete -f -
+
+echo_step "wait for deletion datacenter CR"
+kubectl wait --for=delete datacenters/example
+
+## Uninstalling Crossplane Provider IONOS Cloud
 
 echo_step "uninstalling ${PROJECT_NAME}"
 echo "${INSTALL_YAML}" | "${KUBECTL}" delete -f -
