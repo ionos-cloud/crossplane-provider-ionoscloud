c31154c69fe166f53e90e15d63ea1fa365742207
AnaMariaMoga <anamoga435@gmail.com>
2022-02-23 15:38:37 +0200

    feat: add go generate crossplane

--- apis/compute/v1alpha1/server_cube_types.go
+++ apis/compute/v1alpha1/server_cube_types.go
@@ -17,15 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"context"
 	"reflect"
-
-	"github.com/crossplane/crossplane-runtime/pkg/meta"
-	"github.com/crossplane/crossplane-runtime/pkg/reference"
-	"github.com/crossplane/crossplane-runtime/pkg/resource"
-	"github.com/pkg/errors"
-
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -35,22 +27,32 @@ import (
 
 // CubeServerProperties are the observable fields of a Cube Server.
 type CubeServerProperties struct {
+	// DatacenterID is the ID of the Datacenter on which the server will be created.
+	// It needs to be provided via directly or via reference.
+	//
 	// +immutable
-	DatacenterID string `json:"datacenterID,omitempty"`
+	// +crossplane:generate:reference:type=Datacenter
+	DatacenterID string `json:"datacenterId,omitempty"`
 	// DatacenterRef references to a Datacenter to retrieve its name
 	// +optional
-	DatacenterIDRef *xpv1.Reference `json:"datacenterIDRef,omitempty"`
+	DatacenterIDRef *xpv1.Reference `json:"datacenterIdRef,omitempty"`
+	// DatacenterIDSelector selects reference to a Datacenter to retrieve its datacenterId
+	//
 	// +optional
-	DatacenterIDRefSelector *xpv1.Selector `json:"datacenterIDRefSelector,omitempty"`
+	DatacenterIDSelector *xpv1.Selector `json:"datacenterIdSelector,omitempty"`
 	// The ID of the template for creating a CUBE server; the available templates for CUBE servers can be found on the templates' resource.
+	//
+	// +kubebuilder:validation:Required
 	Template Template `json:"template"`
 	// The name of the  resource.
 	Name string `json:"name,omitempty"`
 	// The availability zone in which the server should be provisioned.
+	//
 	// +kubebuilder:validation:Enum=AUTO;ZONE_1;ZONE_2
 	AvailabilityZone string `json:"availabilityZone,omitempty"`
 	// CPU architecture on which server gets provisioned; not all CPU architectures are available in all datacenter regions;
 	// available CPU architectures can be retrieved from the datacenter resource.
+	//
 	// +kubebuilder:validation:Enum=AMD_OPTERON;INTEL_SKYLAKE;INTEL_XEON
 	CPUFamily string `json:"cpuFamily,omitempty"`
 }
@@ -69,45 +71,12 @@ type CubeServerSpec struct {
 	ForProvider       CubeServerProperties `json:"forProvider"`
 }
 
-// ResolveReferences implements the ReferenceResolver interface for a resource.
-// It's called to resolve references in the server to e.g. the datacenter.
-func (in *CubeServer) ResolveReferences(ctx context.Context, c client.Reader) error {
-	r := reference.NewAPIResolver(c, in)
-	const undefined = ""
-	// Resolve spec.forProvider.datacenterID
-	datacenterID, err := r.Resolve(ctx, reference.ResolutionRequest{
-		CurrentValue: in.Spec.ForProvider.DatacenterID,
-		Reference:    in.Spec.ForProvider.DatacenterIDRef,
-		Selector:     in.Spec.ForProvider.DatacenterIDRefSelector,
-		To:           reference.To{Managed: &Datacenter{}, List: &DatacenterList{}},
-		Extract: func(managed resource.Managed) string {
-			c, ok := managed.(*Datacenter)
-			if !ok {
-				return undefined
-			}
-			if meta.GetExternalName(c) == c.Name {
-				return undefined
-			}
-			return meta.GetExternalName(c)
-		},
-	})
-	if err != nil {
-		return errors.Wrap(err, "spec.forProvider.datacenterID")
-	}
-	if datacenterID.ResolvedValue == undefined {
-		return errors.New("datacenter either not found or not reconciled yet")
-	}
-	in.Spec.ForProvider.DatacenterID = datacenterID.ResolvedValue
-	in.Spec.ForProvider.DatacenterIDRef = datacenterID.ResolvedReference
-	return nil
-}
-
 // +kubebuilder:object:root=true
 
 // A CubeServer is an example API type.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
-// +kubebuilder:printcolumn:name="DATACENTER ID",type="string",JSONPath=".spec.forProvider.datacenterID"
+// +kubebuilder:printcolumn:name="DATACENTER ID",type="string",JSONPath=".spec.forProvider.datacenterId"
 // +kubebuilder:printcolumn:name="ID",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
 // +kubebuilder:printcolumn:name="STATE",type="string",JSONPath=".status.atProvider.state"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
--- apis/compute/v1alpha1/server_types.go
+++ apis/compute/v1alpha1/server_types.go
@@ -17,52 +17,57 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"context"
 	"reflect"
-
-	"github.com/pkg/errors"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-
-	"github.com/crossplane/crossplane-runtime/pkg/meta"
-	"github.com/crossplane/crossplane-runtime/pkg/reference"
-	"github.com/crossplane/crossplane-runtime/pkg/resource"
 
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 )
 
 // ServerProperties are the observable fields of a Server.
 type ServerProperties struct {
+	// DatacenterID is the ID of the Datacenter on which the server will be created.
+	// It needs to be provided via directly or via reference.
+	//
 	// +immutable
-	DatacenterID string `json:"datacenterID,omitempty"`
+	// +crossplane:generate:reference:type=Datacenter
+	DatacenterID string `json:"datacenterId,omitempty"`
 	// DatacenterIDRef references to a Datacenter to retrieve its ID
+	//
 	// +optional
-	DatacenterIDRef *xpv1.Reference `json:"datacenterIDRef,omitempty"`
+	DatacenterIDRef *xpv1.Reference `json:"datacenterIdRef,omitempty"`
+	// DatacenterIDSelector selects reference to a Datacenter to retrieve its datacenterId
+	//
 	// +optional
-	DatacenterIDRefSelector *xpv1.Selector `json:"datacenterIDRefSelector,omitempty"`
+	DatacenterIDSelector *xpv1.Selector `json:"datacenterIdSelector,omitempty"`
 	// The name of the  resource.
 	Name string `json:"name,omitempty"`
 	// The total number of cores for the server.
+	//
+	// +kubebuilder:validation:Required
 	Cores int32 `json:"cores"`
 	// The memory size for the server in MB, such as 2048. Size must be specified in multiples of 256 MB with a minimum of 256 MB.
 	// however, if you set ramHotPlug to TRUE then you must use a minimum of 1024 MB. If you set the RAM size more than 240GB,
 	// then ramHotPlug will be set to FALSE and can not be set to TRUE unless RAM size not set to less than 240GB.
+	//
 	// +kubebuilder:validation:MultipleOf=256
+	// +kubebuilder:validation:Required
 	RAM int32 `json:"ram"`
 	// The availability zone in which the server should be provisioned.
+	//
 	// +kubebuilder:validation:Enum=AUTO;ZONE_1;ZONE_2
 	AvailabilityZone string `json:"availabilityZone,omitempty"`
 	// CPU architecture on which server gets provisioned; not all CPU architectures are available in all datacenter regions;
 	// available CPU architectures can be retrieved from the datacenter resource.
+	//
 	// +kubebuilder:validation:Enum=AMD_OPTERON;INTEL_SKYLAKE;INTEL_XEON
 	CPUFamily string `json:"cpuFamily,omitempty"`
 }
 
 // ServerObservation are the observable fields of a Server.
 type ServerObservation struct {
-	ServerID string `json:"serverID,omitempty"`
+	ServerID string `json:"serverId,omitempty"`
 	State    string `json:"state,omitempty"`
 }
 
@@ -78,45 +83,12 @@ type ServerStatus struct {
 	AtProvider          ServerObservation `json:"atProvider,omitempty"`
 }
 
-// ResolveReferences implements the ReferenceResolver interface for a resource.
-// It's called to resolve references in the server to e.g. the datacenter.
-func (in *Server) ResolveReferences(ctx context.Context, c client.Reader) error {
-	r := reference.NewAPIResolver(c, in)
-	const undefined = ""
-	// Resolve spec.forProvider.datacenterID
-	datacenterID, err := r.Resolve(ctx, reference.ResolutionRequest{
-		CurrentValue: in.Spec.ForProvider.DatacenterID,
-		Reference:    in.Spec.ForProvider.DatacenterIDRef,
-		Selector:     in.Spec.ForProvider.DatacenterIDRefSelector,
-		To:           reference.To{Managed: &Datacenter{}, List: &DatacenterList{}},
-		Extract: func(managed resource.Managed) string {
-			c, ok := managed.(*Datacenter)
-			if !ok {
-				return undefined
-			}
-			if meta.GetExternalName(c) == c.Name {
-				return undefined
-			}
-			return meta.GetExternalName(c)
-		},
-	})
-	if err != nil {
-		return errors.Wrap(err, "spec.forProvider.datacenterID")
-	}
-	if datacenterID.ResolvedValue == undefined {
-		return errors.New("datacenter either not found or not reconciled yet")
-	}
-	in.Spec.ForProvider.DatacenterID = datacenterID.ResolvedValue
-	in.Spec.ForProvider.DatacenterIDRef = datacenterID.ResolvedReference
-	return nil
-}
-
 // +kubebuilder:object:root=true
 
 // A Server is an example API type.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
-// +kubebuilder:printcolumn:name="DATACENTER ID",type="string",JSONPath=".spec.forProvider.datacenterID"
+// +kubebuilder:printcolumn:name="DATACENTER ID",type="string",JSONPath=".spec.forProvider.datacenterId"
 // +kubebuilder:printcolumn:name="ID",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
 // +kubebuilder:printcolumn:name="STATE",type="string",JSONPath=".status.atProvider.state"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
