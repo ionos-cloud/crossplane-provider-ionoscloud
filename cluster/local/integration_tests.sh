740e87818b2a65b519ba2cde9bb664b3e6672a1c
AnaMariaMoga <anamoga435@gmail.com>
2022-03-30 15:12:50 +0300

    tests: add tests for k8s nodepool

--- cluster/local/integration_tests.sh
+++ cluster/local/integration_tests.sh
@@ -27,11 +27,11 @@ CONTROLLER_IMAGE="${REGISTRY}/${ORG_NAME}/${PROJECT_NAME}-controller"
 # Pay attention to the default values that are set!
 # To run specific tests, for example for dbaas resources,
 # use: make e2e TEST_COMPUTE=false TEST_DBAAS=true
-TEST_COMPUTE=${TEST_COMPUTE:-true}
+TEST_COMPUTE=${TEST_COMPUTE:-false }
 # by default, do not test the following resources
 # since it takes a lot of time
 TEST_DBAAS=${TEST_DBAAS:-false}
-TEST_K8S=${TEST_K8S:-false}
+TEST_K8S=${TEST_K8S:-true}
 
 version_tag="$(cat ${projectdir}/_output/version)"
 # tag as latest version to load into kind cluster
@@ -174,6 +174,8 @@ fi
 if [ "$TEST_K8S" = true ]; then
   echo_step "--- k8s cluster tests ---"
   k8s_cluster_tests
+  echo_step "--- k8s nodepool tests ---"
+  k8s_nodepool_tests
 fi
 
 echo_step "-------------------"
@@ -201,6 +203,8 @@ if [ "$TEST_DBAAS" = true ]; then
 fi
 
 if [ "$TEST_K8S" = true ]; then
+  echo_step "--- k8s nodepool tests ---"
+  k8s_nodepool_tests_cleanup
   echo_step "--- k8s cluster tests ---"
   k8s_cluster_tests_cleanup
 fi
