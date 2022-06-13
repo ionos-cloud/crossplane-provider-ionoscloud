c348c296153be268263b07431e567d07d0196056
Christoph Petrausch <christoph.petrausch@inovex.de>
2022-06-13 11:44:22 +0200

    Move CPU family from late init to status.
    The old implementation will cause endless churn loops, if the nodepool resource is created by
    another reconciler which has the empty string as desired value.
    This is the case if the user uses ArgoCD or fluxCD.

--- apis/generate.go
+++ apis/generate.go
@@ -1,3 +1,4 @@
+//go:build generate
 // +build generate
 
 /*
@@ -20,7 +21,7 @@ limitations under the License.
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
 // Remove existing CRDs
-//go:generate rm -rf ../package/crds
+//go:generate rm -rf ../package/crds/*.yaml
 
 // Generate deepcopy methodsets and CRD manifests
 //go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=./... crd:trivialVersions=true,allowDangerousTypes=true,crdVersions=v1 output:artifacts:config=../package/crds
