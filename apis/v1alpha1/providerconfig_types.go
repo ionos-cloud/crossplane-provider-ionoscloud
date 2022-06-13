302c1925258798702aa3a509e0144fccdcae0a65
Christoph Petrausch <christoph.petrausch@inovex.de>
2022-06-13 09:22:04 +0200

    Add correct categories to the providerconfig types

--- apis/v1alpha1/providerconfig_types.go
+++ apis/v1alpha1/providerconfig_types.go
@@ -52,6 +52,7 @@ type ProviderConfigStatus struct {
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentials.secretRef.name",priority=1
 // +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,ionoscloud}
 type ProviderConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
