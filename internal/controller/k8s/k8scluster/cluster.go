d2a05551ed2680e4bbdb0f7b4ae34b8d7fa531cd
amoga-ionos <anamoga435@gmail.com>
2022-06-17 17:11:59 +0300

    fix: k8s resources

--- internal/controller/k8s/k8scluster/cluster.go
+++ internal/controller/k8s/k8scluster/cluster.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -45,14 +44,7 @@ import (
 	"github.com/ionos-cloud/crossplane-provider-ionoscloud/internal/clients/k8s/k8scluster"
 )
 
-const (
-	errNotK8sCluster = "managed resource is not a K8s Cluster custom resource"
-	errTrackPCUsage  = "cannot track ProviderConfig usage"
-	errGetPC         = "cannot get ProviderConfig"
-	errGetCreds      = "cannot get credentials"
-
-	errNewClient = "cannot create new Service"
-)
+const errNotK8sCluster = "managed resource is not a K8s Cluster custom resource"
 
 // Setup adds a controller that reconciles K8sCluster managed resources.
 func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, poll time.Duration, creationGracePeriod time.Duration) error {
@@ -96,27 +88,10 @@ func (c *connectorCluster) Connect(ctx context.Context, mg resource.Managed) (ma
 	if !ok {
 		return nil, errors.New(errNotK8sCluster)
 	}
-
-	if err := c.usage.Track(ctx, mg); err != nil {
-		return nil, errors.Wrap(err, errTrackPCUsage)
-	}
-
-	pc := &apisv1alpha1.ProviderConfig{}
-	if err := c.kube.Get(ctx, types.NamespacedName{Name: mg.GetProviderConfigReference().Name}, pc); err != nil {
-		return nil, errors.Wrap(err, errGetPC)
-	}
-
-	cd := pc.Spec.Credentials
-	data, err := resource.CommonCredentialExtractor(ctx, cd.Source, c.kube, cd.CommonCredentialSelectors)
-	if err != nil {
-		return nil, errors.Wrap(err, errGetCreds)
-	}
-
-	svc, err := clients.NewIonosClients(data)
-	if err != nil {
-		return nil, errors.Wrap(err, errNewClient)
-	}
-	return &externalCluster{service: &k8scluster.APIClient{IonosServices: svc}, log: c.log}, nil
+	svc, err := clients.ConnectForCRD(ctx, mg, c.kube, c.usage)
+	return &externalCluster{
+		service: &k8scluster.APIClient{IonosServices: svc},
+		log:     c.log}, err
 }
 
 // An ExternalClient observes, then either creates, updates, or deletes an
