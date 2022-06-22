8045b9922c156df8b15780bc5b4281b694a9ac07
amoga-ionos <anamoga435@gmail.com>
2022-06-22 16:48:52 +0300

    feat: first draft for generator of documentation

--- tools/doc.go
+++ tools/doc.go
@@ -1,22 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io"
+	"os"
+
 	"github.com/ionos-cloud/crossplane-provider-ionoscloud/package/crds"
 )
 
 func main() {
-	//dir := os.Getenv("DOCS_OUT")
-	//if dir == "" {
-	//	fmt.Printf("DOCS_OUT environment variable not set.\n")
-	//	os.Exit(1)
-	//}
-	//if _, err := os.Stat(dir); err != nil {
-	//	fmt.Printf("Error getting directory: %v\n", err)
-	//	os.Exit(1)
-	//}
+	f, err := os.Create("test.md")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = writeContent(f)
+	if err != nil {
+		panic(err)
+	}
+}
 
+func writeContent(w io.Writer) error {
+	buf := new(bytes.Buffer)
 	mustGetCRDs := crds.MustGetCRDs()
-	fmt.Println(mustGetCRDs[0].Spec.Versions[0].Schema.OpenAPIV3Schema.Properties["spec"].Properties["forProvider"].Properties["name"].Description)
-	fmt.Println(mustGetCRDs[0].Spec.Versions[0].Schema.OpenAPIV3Schema.Properties["spec"].Properties["forProvider"].Required)
+	buf.WriteString("# " + mustGetCRDs[0].Spec.Names.Kind + " Managed Resource\n\n")
+	buf.WriteString("# Overview\n\n")
+	buf.WriteString("* Resource Name: " + mustGetCRDs[0].Spec.Names.Kind + "\n")
+	buf.WriteString("* Resource Group: " + mustGetCRDs[0].Spec.Group + "\n")
+	buf.WriteString("* Resource Version: " + mustGetCRDs[0].Spec.Versions[0].Name + "\n")
+	buf.WriteString("* Resource Scope: " + string(mustGetCRDs[0].Spec.Scope) + "\n\n")
+	buf.WriteString("## Properties:\n\n")
+	for key, requiredValue := range mustGetCRDs[0].Spec.Versions[0].Schema.OpenAPIV3Schema.Properties["spec"].Properties["forProvider"].Properties {
+		buf.WriteString("* " + key + "\n")
+		buf.WriteString("	* description: " + requiredValue.Description + "\n")
+		buf.WriteString("	* type: " + requiredValue.Type + "\n")
+		buf.WriteString("	* default: " + requiredValue.Default.String() + "\n")
+		buf.WriteString("	* format: " + requiredValue.Format + "\n")
+		buf.WriteString("	* pattern: " + requiredValue.Pattern + "\n")
+		// Check all validations added on apis_types
+	}
+	buf.WriteString("\n")
+	buf.WriteString("## Required Properties:\n")
+	for _, requiredValue := range mustGetCRDs[0].Spec.Versions[0].Schema.OpenAPIV3Schema.Properties["spec"].Properties["forProvider"].Required {
+		buf.WriteString("* " + requiredValue + "\n")
+	}
+	// add entire CRD - link
+	// add example - link
+	// add kubectl commands - usage
+	_, err := buf.WriteTo(w)
+	return err
 }
