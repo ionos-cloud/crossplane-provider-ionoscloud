f8bf90b38240aafe229c436d3b652611135f294b
Christoph Petrausch <christoph.petrausch@inovex.de>
2022-06-22 22:58:31 +0200

    Add unit tests for k8s nodepools

---
+++ internal/mock/clients/compute/datacenter/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run github.com/golang/mock/mockgen -source=../../../../clients/compute/datacenter/datacenter.go -destination=datacenter.go -package datacenter
+
+package datacenter
---
+++ internal/mock/clients/compute/ipblock/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run github.com/golang/mock/mockgen -source=../../../../clients/compute/ipblock/ipblock.go -destination=ipblock.go -package ipblock
+
+package ipblock
