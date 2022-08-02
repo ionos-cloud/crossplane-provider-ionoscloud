5c96f9af07b01c2b22e8e96db62aea2905cc1e88
Ana-Maria Moga <59568286+amoga-ionos@users.noreply.github.com>
2022-08-02 17:16:55 +0300

    fix: cleanup test account workflow (#97)

--- tools/scripts/cleanup/delete-all-resources.sh
+++ tools/scripts/cleanup/delete-all-resources.sh
@@ -8,11 +8,8 @@
 # load delete functions
 source ./delete-backup-resources.sh
 source ./delete-dbaas-resources.sh
-source ./delete-user-management-resources.sh
-source ./delete-nat-gateway-resources.sh
 source ./delete-k8s-resources.sh
 source ./delete-compute-engine-resources.sh
-source ./delete-nlb-resources.sh
 
 # load print utils
 source ./print-utils.sh
@@ -24,17 +21,8 @@ delete_backup_resources
 echo_step "starting cleanup on Database as a Service resources"
 delete_dbaas_resources
 
-echo_step "starting cleanup on User Management resources"
-delete_user_management_resources
-
 echo_step "starting cleanup on Managed Kubernetes resources"
 delete_k8s_resources
-
-echo_step "starting cleanup on NAT Gateway resources"
-delete_nat_gateway_resources
-
-echo_step "starting cleanup on Network Load Balancer resources"
-delete_nlb_resources
 
 echo_step "starting cleanup on Compute Engine resources"
 delete_compute_engine_resources
