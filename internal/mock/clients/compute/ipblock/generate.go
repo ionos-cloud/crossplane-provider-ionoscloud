a65f7153cc7b6978fab56fa7ef927a61eab09e3e
Wesley Mendes <wesley.mendes@ionos.com>
2024-02-02 12:59:28 +0100

    feat: reduce duplication on mock generation.
    Uses only one generate file to config gomock for clients.

--- internal/mock/clients/compute/ipblock/generate.go
+++
@@ -1,3 +0,0 @@
-//go:generate go run github.com/golang/mock/mockgen -source=../../../../clients/compute/ipblock/ipblock.go -destination=ipblock.go -package ipblock
-
-package ipblock
--- internal/mock/clients/k8s/k8snodepool/generate.go
+++
@@ -1,3 +0,0 @@
-//go:generate go run github.com/golang/mock/mockgen -source=../../../../clients/k8s/k8snodepool/nodepool.go -destination=nodepool.go -package k8snodepool
-
-package k8snodepool
