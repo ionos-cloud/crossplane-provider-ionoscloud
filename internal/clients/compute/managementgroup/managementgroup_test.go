252a88f30276944f220d04326bdee24fab94abfb
ionutb <corneliu-ionut.beti@ionos.com>
2024-02-23 11:36:53 +0200

    test: group unit tests

--- internal/clients/compute/managementgroup/managementgroup_test.go
+++ internal/clients/compute/managementgroup/managementgroup_test.go
@@ -1,1 +1,176 @@
 package managementgroup
+
+import (
+	"testing"
+
+	"github.com/ionos-cloud/crossplane-provider-ionoscloud/apis/compute/v1alpha1"
+	"github.com/ionos-cloud/crossplane-provider-ionoscloud/internal/utils"
+	psql "github.com/ionos-cloud/sdk-go-dbaas-postgres"
+	ionoscloud "github.com/ionos-cloud/sdk-go/v6"
+)
+
+func TestIsManagementGroupUpToDate(t *testing.T) {
+
+	type args struct {
+		cr        *v1alpha1.ManagementGroup
+		Group     ionoscloud.Group
+		memberIDs utils.Set[string]
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "both empty",
+			args: args{
+				cr:    nil,
+				Group: ionoscloud.Group{},
+			},
+			want: true,
+		},
+		{
+			name: "cr empty",
+			args: args{
+				cr:    nil,
+				Group: ionoscloud.Group{Properties: &ionoscloud.GroupProperties{Name: psql.ToPtr("meow")}},
+			},
+			want: false,
+		},
+		{
+			name: "observed empty",
+			args: args{
+				cr: &v1alpha1.ManagementGroup{
+					Spec: v1alpha1.ManagementGroupSpec{
+						ForProvider: v1alpha1.ManagementGroupParameters{
+							Name: "foo",
+						},
+					},
+				},
+				Group: ionoscloud.Group{},
+			},
+			want: false,
+		},
+		{
+			name: "equal properties",
+			args: args{
+				cr: &v1alpha1.ManagementGroup{
+					Spec: v1alpha1.ManagementGroupSpec{
+						ForProvider: v1alpha1.ManagementGroupParameters{
+							Name:                 "foo",
+							CreateDataCenter:     true,
+							CreateInternetAccess: true,
+							ReserveIP:            true,
+							CreateK8sCluster:     true,
+						},
+					},
+				},
+				Group: ionoscloud.Group{Properties: &ionoscloud.GroupProperties{
+					Name:                 psql.ToPtr("foo"),
+					CreateDataCenter:     psql.ToPtr(true),
+					CreateInternetAccess: psql.ToPtr(true),
+					ReserveIp:            psql.ToPtr(true),
+					CreateK8sCluster:     psql.ToPtr(true),
+				},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "different properties",
+			args: args{
+				cr: &v1alpha1.ManagementGroup{
+					Spec: v1alpha1.ManagementGroupSpec{
+						ForProvider: v1alpha1.ManagementGroupParameters{
+							Name:                 "meow",
+							CreateDataCenter:     true,
+							CreateInternetAccess: false,
+							ReserveIP:            true,
+							CreateK8sCluster:     true,
+						},
+					},
+				},
+				Group: ionoscloud.Group{Properties: &ionoscloud.GroupProperties{
+					Name:                 psql.ToPtr("foo"),
+					CreateDataCenter:     psql.ToPtr(true),
+					CreateInternetAccess: psql.ToPtr(true),
+					ReserveIp:            psql.ToPtr(true),
+					CreateK8sCluster:     psql.ToPtr(false),
+				},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "equal properties and members",
+			args: args{
+				cr: &v1alpha1.ManagementGroup{
+					Spec: v1alpha1.ManagementGroupSpec{
+						ForProvider: v1alpha1.ManagementGroupParameters{
+							Name:                 "foo",
+							CreateDataCenter:     true,
+							CreateInternetAccess: true,
+							ReserveIP:            true,
+							CreateK8sCluster:     true,
+							UserCfg: []v1alpha1.UserConfig{
+								{UserID: "17dc05fa-8e39-4d68-9529-5a428494882a"},
+								{UserID: "338fa0ac-ab6e-4add-90d9-4850b743371f"},
+								{UserID: "777c08be-1c95-4b6f-a15d-02994f3de1a1"},
+							},
+						},
+					},
+				},
+				Group: ionoscloud.Group{Properties: &ionoscloud.GroupProperties{
+					Name:                 psql.ToPtr("foo"),
+					CreateDataCenter:     psql.ToPtr(true),
+					CreateInternetAccess: psql.ToPtr(true),
+					ReserveIp:            psql.ToPtr(true),
+					CreateK8sCluster:     psql.ToPtr(true),
+				},
+				},
+				memberIDs: utils.NewSetFromSlice([]string{
+					"777c08be-1c95-4b6f-a15d-02994f3de1a1",
+					"338fa0ac-ab6e-4add-90d9-4850b743371f",
+					"17dc05fa-8e39-4d68-9529-5a428494882a",
+				}),
+			},
+			want: true,
+		},
+
+		{
+			name: "different members",
+			args: args{
+				cr: &v1alpha1.ManagementGroup{
+					Spec: v1alpha1.ManagementGroupSpec{
+						ForProvider: v1alpha1.ManagementGroupParameters{
+							Name: "meow",
+							UserCfg: []v1alpha1.UserConfig{
+								{UserID: "17dc05fa-8e39-4d68-9529-5a428494882a"},
+								{UserID: "338fa0ac-ab6e-4add-90d9-4850b743371f"},
+								{UserID: "777c08be-1c95-4b6f-a15d-02994f3de1a1"},
+							},
+						},
+					},
+				},
+				Group: ionoscloud.Group{Properties: &ionoscloud.GroupProperties{
+					Name: psql.ToPtr("meow"),
+				},
+				},
+				memberIDs: utils.NewSetFromSlice([]string{
+					"17dc05fa-8e39-4d68-9529-5a428494882a",
+					"338fa0ac-ab6e-4add-90d9-4850b743371f",
+				}),
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsManagementGroupUpToDate(tt.args.cr, tt.args.Group, tt.args.memberIDs); got != tt.want {
+				t.Errorf("IsManagementGroupUpToDate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
