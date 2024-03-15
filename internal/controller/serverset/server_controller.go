2b98ebebe379a295431e9466683dd9c61f43c19b
cristi <cristian.mihai.guran@ionos.com>
2024-03-15 12:38:48 +0200

    feat: index 0 should return zone_1

--- internal/controller/serverset/server_controller.go
+++ internal/controller/serverset/server_controller.go
@@ -144,9 +144,9 @@ func fromServerSetToServer(cr *v1alpha1.ServerSet, replicaIndex, version, volume
 		}}
 }
 
-// GetZoneFromIndex returns ZONE_1 for odd and ZONE_2 for even index
+// GetZoneFromIndex returns ZONE_2 for odd and ZONE_1 for even index
 func GetZoneFromIndex(index int) string {
-	return fmt.Sprintf("ZONE_%d", (index+1)%2+1)
+	return fmt.Sprintf("ZONE_%d", index%2+1)
 }
 
 func (k *kubeServerController) Ensure(ctx context.Context, cr *v1alpha1.ServerSet, replicaIndex, version, volumeVersion int) error {
