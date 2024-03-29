862dd2dc5581004bc5426efb9dd8c65fe21319b8
cguran-ionos <cristian-mihai.guran@ionos.com>
2024-03-29 09:59:40 +0200

    Feat/add pwd ssh keys validations (#189)

--- apis/compute/v1alpha1/volumeselector_types.go
+++ apis/compute/v1alpha1/volumeselector_types.go
@@ -62,7 +62,7 @@ type VolumeselectorObservation struct {
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,ionoscloud}
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,ionoscloud},shortName=vs;volsel
 type Volumeselector struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
