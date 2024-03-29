fd573f9d831da97b5b2d028c84cd9c25344394d0
cristi <cristian.mihai.guran@ionos.com>
2024-03-29 12:41:50 +0200

    feat: merge from branch

--- internal/controller/serverset/serverset.go
+++ internal/controller/serverset/serverset.go
@@ -505,8 +505,8 @@ func AreNICsUpToDate(ctx context.Context, kube client.Client, serversetName stri
 	return true, nil
 }
 
-// GetServersFromServerSet - gets servers from a server set based on the ionoscloud.com/serverset label
-func GetServersFromServerSet(ctx context.Context, kube client.Client, name string) ([]v1alpha1.Server, error) {
+// GetServersOfSSet - gets servers from a server set based on the ionoscloud.com/serverset label
+func GetServersOfSSet(ctx context.Context, kube client.Client, name string) ([]v1alpha1.Server, error) {
 	serverList := &v1alpha1.ServerList{}
 	if err := kube.List(ctx, serverList, client.MatchingLabels{
 		serverSetLabel: name,
@@ -517,8 +517,8 @@ func GetServersFromServerSet(ctx context.Context, kube client.Client, name strin
 	return serverList.Items, nil
 }
 
-// GetVolumesFromServerSet - gets volumes from a server set based on the ionoscloud.com/serverset label
-func GetVolumesFromServerSet(ctx context.Context, kube client.Client, name string) ([]v1alpha1.Volume, error) {
+// GetVolumesOfSSet - gets volumes from a server set based on the ionoscloud.com/serverset label
+func GetVolumesOfSSet(ctx context.Context, kube client.Client, name string) ([]v1alpha1.Volume, error) {
 	volumeList := &v1alpha1.VolumeList{}
 	if err := kube.List(ctx, volumeList, client.MatchingLabels{
 		serverSetLabel: name,
