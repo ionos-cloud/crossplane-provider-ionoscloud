4e92c026e622492ee05fb73f9822f77af0b557d9
fghimpeteanu <filip.ghimpateanu@ionos.com>
2024-04-24 10:26:13 +0300

    feat: add dhcpv6 field

--- apis/compute/v1alpha1/serverset_types.go
+++ apis/compute/v1alpha1/serverset_types.go
@@ -80,6 +80,7 @@ type ServerSetTemplateSpec struct {
 }
 
 // ServerSetTemplateNIC are the configurable fields of a ServerSetTemplateNIC.
+// +kubebuilder:validation:XValidation:rule="(self.dhcp == false && self.dhcpv6 == false) || (self.dhcp != self.dhcpv6)", message="Only one of 'dhcp' or 'dhcpv6' can be set to true"
 type ServerSetTemplateNIC struct {
 	// Name of the NIC. Replica index, NIC index, and version are appended to the name. Resulting name will be in format: {name}-{replicaIndex}-{nicIndex}-{version}.
 	// Version increases if the NIC is re-created due to an immutable field changing. E.g. if the bootvolume type or image are changed and the strategy is createAllBeforeDestroy, the NIC is re-created and the version is increased.
@@ -92,6 +93,8 @@ type ServerSetTemplateNIC struct {
 	IPv4 string `json:"ipv4,omitempty"`
 	// +kubebuilder:validation:Required
 	DHCP bool `json:"dhcp"`
+	// +kubebuilder:validation:Required
+	DHCPv6 bool `json:"dhcpv6"`
 	// +kubebuilder:validation:Optional
 	VNetID string `json:"vnetId,omitempty"`
 	// +kubebuilder:validation:Required
