5f2137f8c5eae1445cd2ac9d0e515bbe45898136
cristi <cristian.mihai.guran@ionos.com>
2024-08-13 15:56:46 +0300

    fix: nolint

--- internal/controller/serverset/bootvolume_controller.go
+++ internal/controller/serverset/bootvolume_controller.go
@@ -35,7 +35,7 @@ type kubeBootVolumeController struct {
 }
 
 // Create creates a volume CR and waits until in reaches AVAILABLE state
-func (k *kubeBootVolumeController) Create(ctx context.Context, cr *v1alpha1.ServerSet, replicaIndex, version int) (v1alpha1.Volume, error) { // nolint:gocyclo
+func (k *kubeBootVolumeController) Create(ctx context.Context, cr *v1alpha1.ServerSet, replicaIndex, version int) (v1alpha1.Volume, error) {
 	name := getNameFrom(cr.Spec.ForProvider.BootVolumeTemplate.Metadata.Name, replicaIndex, version)
 	hostname := getNameFrom(cr.Spec.ForProvider.Template.Metadata.Name, replicaIndex, version)
 	k.log.Info("Creating BootVolume", "name", name)
@@ -68,7 +68,7 @@ func (k *kubeBootVolumeController) Create(ctx context.Context, cr *v1alpha1.Serv
 // one global state where to hold used ip addressed for substitutions for each statefulserverset
 var globalStateMap = make(map[string]substitution.GlobalState)
 
-func (k *kubeBootVolumeController) setPatcher(ctx context.Context, cr *v1alpha1.ServerSet, replicaIndex, version int, name string, kube client.Client) (*ccpatch.CloudInitPatcher, error) {
+func (k *kubeBootVolumeController) setPatcher(ctx context.Context, cr *v1alpha1.ServerSet, replicaIndex, version int, name string, kube client.Client) (*ccpatch.CloudInitPatcher, error) { // nolint:gocyclo
 	var userDataPatcher *ccpatch.CloudInitPatcher
 	var err error
 	userData := cr.Spec.ForProvider.BootVolumeTemplate.Spec.UserData
