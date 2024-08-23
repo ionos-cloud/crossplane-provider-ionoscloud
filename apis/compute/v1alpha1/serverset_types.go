bd1747d00078579939c241a0a3c9f45ceb5804da
cristi <cristian.mihai.guran@ionos.com>
2024-08-23 13:07:27 +0300

    fix: remove check for cpu family type

--- apis/compute/v1alpha1/serverset_types.go
+++ apis/compute/v1alpha1/serverset_types.go
@@ -63,7 +63,6 @@ type ServerSetTemplateSpec struct {
 	// available CPU architectures can be retrieved from the datacenter resource.
 	//
 	// +immutable
-	// +kubebuilder:validation:Enum=AMD_OPTERON;INTEL_SKYLAKE;INTEL_XEON
 	CPUFamily string `json:"cpuFamily,omitempty"`
 	// The total number of cores for the server.
 	//
