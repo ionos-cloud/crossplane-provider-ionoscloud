14e2b36cae110d0820be1e3f4410fd77d2c03a8a
Willi Kahlmann <willi.kahlmann@ionos.com>
2024-11-26 16:01:09 +0100

    serverset: Set Zone to AUTO

--- internal/controller/serverset/server_controller_test.go
+++ internal/controller/serverset/server_controller_test.go
@@ -16,35 +16,40 @@ func TestGetZoneFromIndex(t *testing.T) {
 			args: args{
 				index: 0,
 			},
-			want: "ZONE_1",
+			want: "AUTO",
+			// want: "ZONE_1",
 		},
 		{
 			name: "index1ExpectedZone_2",
 			args: args{
 				index: 1,
 			},
-			want: "ZONE_2",
+			want: "AUTO",
+			// want: "ZONE_2",
 		},
 		{
 			name: "index2ExpectedZone_1",
 			args: args{
 				index: 2,
 			},
-			want: "ZONE_1",
+			want: "AUTO",
+			// want: "ZONE_1",
 		},
 		{
 			name: "index10ExpectedZone_1",
 			args: args{
 				index: 10,
 			},
-			want: "ZONE_1",
+			want: "AUTO",
+			// want: "ZONE_1",
 		},
 		{
 			name: "index111ExpectedZone_2",
 			args: args{
 				index: 111,
 			},
-			want: "ZONE_2",
+			want: "AUTO",
+			// want: "ZONE_2",
 		},
 	}
 	for _, tt := range tests {
