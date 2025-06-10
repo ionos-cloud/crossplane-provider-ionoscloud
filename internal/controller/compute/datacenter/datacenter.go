965f8557cace3340cbd015e1d3563321f4060fec
cristi <cristian.mihai.guran@ionos.com>
2025-06-10 17:56:44 +0300

    fix: remove space

--- internal/controller/compute/datacenter/datacenter.go
+++ internal/controller/compute/datacenter/datacenter.go
@@ -139,7 +139,7 @@ func (c *externalDatacenter) Observe(ctx context.Context, mg resource.Managed) (
 	cr.Status.AtProvider.DatacenterID = meta.GetExternalName(cr)
 	cr.Status.AtProvider.State = clients.GetCoreResourceState(&instance)
 	cr.Status.AtProvider.AvailableCPUFamilies, _ = c.service.GetCPUFamiliesForDatacenter(ctx, cr.Status.AtProvider.DatacenterID)
-	c.log.Debug("Observed datacenter : ", "state", cr.Status.AtProvider.State, "external name", meta.GetExternalName(cr), "name", cr.Spec.ForProvider.Name)
+	c.log.Debug("Observed datacenter: ", "state", cr.Status.AtProvider.State, "external name", meta.GetExternalName(cr), "name", cr.Spec.ForProvider.Name)
 	clients.UpdateCondition(cr, cr.Status.AtProvider.State)
 
 	return managed.ExternalObservation{
